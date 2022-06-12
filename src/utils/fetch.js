9db22a5eb0a9c2e070059194c5599b70e35909b7
linliliya <107317314+linliliya@users.noreply.github.com>
2022-06-12 14:45:56 -0400

    add notify

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,30 @@
 import axios from "axios";
 
 export const sendEmail = (data) => {
-  let api_url = "";
+  let api_url =
+    "https://service-01p4lzv3-1257138158.sh.apigw.tencentcs.com:443/send_email";
+
+  console.log(data);
+  const config = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+
+  return axios
+    .post(api_url, data, config)
+    .then((response) => {
+      console.log(response);
+      return response;
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+};
+
+export const notify = (data) => {
+  let api_url =
+    "https://service-01p4lzv3-1257138158.sh.apigw.tencentcs.com:443/notify";
 
   console.log(data);
   const config = {
